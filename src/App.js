652b580a9ad589cddee291a0982097ecafbe5dbb
Carlos Diaz <carlos9559@gmail.com>
2023-07-13 14:13:11 -0500

    Input field deleted

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todot: [],
+      inputVal: "",
+    };
+  }
+  handleKeyUp = (e) => {
+    if (e.key === "Enter") {
+      this.setState((prevState) => ({
+        todot: [...prevState.todot, prevState.inputVal],
+        inputVal: "",
+      }));
+      e.preventDefault();
+    }
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      inputVal: e.target.value,
+    });
+    console.log(this.state.todot);
+  };
   render() {
     return (
       <div className="wrapper">
@@ -12,15 +35,24 @@ class App extends Component {
             <li>Sacar la ropa</li>
             <li>Hacer la cama</li>
             <li>Leer un rato</li>
+            {this.state.todot.map((element) => (
+              <li>{element}</li>
+            ))}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
+          <form>
+            <input
+              type="text"
+              id="new-task"
+              placeholder="Input todo"
+              onKeyDown={this.handleKeyUp}
+              onChange={this.handleChange}
+              value={this.state.inputVal}
+            />
+          </form>
         </div>
       </div>
-    )
+    );
   }
 }
-
 
 export default App;
